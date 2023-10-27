c19c9fdeb4c361692fa9da63483c621dc49cc24a
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-27 20:27:52 -0300

    feature(UIKit): Creating mock UIKit View

--- SwiftUIInUIKit/ViewController.swift
+++ SwiftUIInUIKit/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Show SwiftUI", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
 
-
+    @objc func didTapButton(){
+        
+    }
+    
 }
 
